92215d993f7239016c1971b544ca898843700644
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-06 22:53:40 -0400

    Create print_vdso_lame.c

---
+++ print_vdso_lame.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
+#include <link.h>      // for AT_SYSINFO_EHDR
+#include <sys/auxv.h>
+
+int main(void) {
+    // Method A: via dlsym
+    void *h = dlopen("linux-vdso.so.1", RTLD_NOW);
+    void *sym = dlsym(h, "__vdso_lame_entry");
+    printf("via dlsym:    __vdso_lame_entry = %p\n", sym);
+
+    // Method B: via getauxval + symbol offset
+    Elf64_Ehdr *vdso_base = (Elf64_Ehdr *)getauxval(AT_SYSINFO_EHDR);
+    printf("vDSO base via auxv: %p\n", vdso_base);
+
+    // parse the ELF in memory to find sym->st_value,
+    printf("via auxv:    %p\n", vdso_base + sym->st_value);
+
+    return 0;
+}
