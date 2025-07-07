0407ac35813522f3fef6ca6bc31ec96c31b8ca4a
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-07 15:14:08 -0400

    direct print

--- print_vdso_lame.c
+++ print_vdso_lame.c
@@ -3,12 +3,15 @@
 #include <dlfcn.h>
 #include <link.h>      // for AT_SYSINFO_EHDR
 #include <sys/auxv.h>
+extern void __vdso_lame_entry(void);
 
 int main(void) {
     // Method A: via dlsym
     void *h = dlopen("linux-vdso.so.1", RTLD_NOW);
     void *sym = dlsym(h, "__vdso_lame_entry");
     printf("via dlsym:    __vdso_lame_entry = %p\n", sym);
+
+    printf("via direct print： __vdso_lame_entry = %p\n", (void *)__vdso_lame_entry);
 
     // Method B: via getauxval + symbol offset
     Elf64_Ehdr *vdso_base = (Elf64_Ehdr *)getauxval(AT_SYSINFO_EHDR);
