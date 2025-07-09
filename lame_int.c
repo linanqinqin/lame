ec1ac8b7756fc1a31fd0401226e72b57882d4291
Nanqinqin Li <linanqinqin@princeton.edu>
2025-07-09 13:58:58 -0400

    lame vector 22

--- lame_int.c
+++ lame_int.c
@@ -6,12 +6,12 @@ int main() {
     int result = __vdso_lame_add(0, 0);
     printf("lame_add = %x\n", result);
 
-    printf("About to invoke LAME interrupt (INT 0xF1)\n");
+    printf("About to invoke LAME interrupt (INT 22)\n");
 
     // Inline assembly to execute INT 0xF1
-    asm volatile ("int $0xF1");
+    asm volatile ("int $22");
 
-    printf("Returned from LAME interrupt (INT 0xF1)\n");
+    printf("Returned from LAME interrupt (INT 22)\n");
 
     return 0;
 }
